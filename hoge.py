bae0cd68563d0ebafb3993fe14a0a12dafc5176c
tookubo <annexdev02ookubo@gmail.com>
2016-01-05 12:51:56 +0900

    Update hoge.py

--- hoge.py
+++ hoge.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# assert 文をシンプルに書ける ok_ eq_ メソッドの読み込み
+from nose.tools import ok_, eq_
 
-class Hoge(object):
+# テストケース
+# test で始まる名前の関数で書けばOK
+def test_1_is_true():
+    ok_(bool(1))
 
-    def __init__(self, v):
-        self.val = v
-
-    def update(self, v):
-        self.val = v
+def test_1_plus_2():
+    eq_(1 + 2, 3)
 
 
